fae9ac6beaf51d5c7fe14d994b3c37f75514c6a3
Rumpels95 <joan.quispec@gmail.com>
2022-07-04 18:02:20 -0500

    Adding project list

---
+++ src/helpers/ProjectList.js
@@ -0,0 +1,28 @@
+import Proj1 from "../assets/foodApp.jpg";
+import Proj2 from "../assets/rocket.jpg";
+import Proj3 from "../assets/fishCount.jpg";
+
+export const ProjectList = [
+  {
+    name: "Food App",
+    image: Proj1,
+    skills: "JavaScript,HTML,CSS",
+    deploy: 'https://foodrecipes-kappa.vercel.app/',
+    githubUrl: ''
+  },
+  {
+    name: "Jon Offer App",
+    image: Proj2,
+    skills: "React,Node.js,MongoDB",
+    deploy: '',
+    githubUrl: ''
+  },
+  {
+    name: "Fish Counting",
+    image: Proj3,
+    skills: "Python, OpenCV",
+    deploy: '',
+    githubUrl: ''
+  },
+  
+];
